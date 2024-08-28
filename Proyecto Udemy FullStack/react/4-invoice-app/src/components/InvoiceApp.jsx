76915db273b537323d6c99a951193280d67a9084
Dyllan Salgado Espinoza <dyllan.salgado@usach.cl>
2024-08-28 15:50:12 -0400

    Se crea todo una app con un solo componente con bootstrap

--- Proyecto Udemy FullStack/react/4-invoice-app/src/components/InvoiceApp.jsx
+++ Proyecto Udemy FullStack/react/4-invoice-app/src/components/InvoiceApp.jsx
@@ -12,50 +12,61 @@ export const InvoiceApp = () => {
 
     return (
         <>
-            {/* Este es un comentario en JSX */}
-            <h1> Ejemplo de factura </h1>
-            <ul>
-                <li> Id: {id}</li>
-                <li> Nombre: {name}</li>
-            </ul>
-            <h3> Datos del Cliente </h3>
-            <ul>
-                <li>Nombre del cliente: {nameClient} {lastName}</li>
-                <li>Dirección: {street} {number}, {city} de {country}</li>
-            </ul>
-            <h3> Datos de Compañia</h3>
-            <ul>
-                <li>Nombre de la Compañia: {nameCompany}</li>
-                <li>Número Fiscal: {fiscalNumber}</li>
-            </ul>
-            <h4> Productos de la Factura</h4>
-            <table>
-                {/* thead delimita los encabezados de la tabla */}
-                <thead>
-                    <tr>
-                        {/* th define las columnas */}
-                        <th>Nombre del Producto</th>
-                        <th>Precio del Producto</th>
-                        <th>Cantidad de Productos disponibles</th>
-                    </tr>
-                </thead>
-                {/* tbody delimita los datos que no son el encabezado */}
-                <tbody>
-                    {/* Se crea un map con las variables deben ir dentro de({}). Tambien se puede usar llamando
-                    una variable x por ejemplo items.map (item => y el return pero dentro de las celdas se deben llamar como
-                    item.producto, item.price.....) */}
-                    {items.map(({id,producto,price,quantity}) =>{
-                        return (
-                            <tr key={ id }>
-                                <td>{producto}</td>
-                                <td>{price}</td>
-                                <td>{quantity}</td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
-                
+            <div className="container">
+                <div className="card my-3">
+                    <div className="card-header"> Ejemplo de factura </div>
+                    <div className="card-body">
+                        <ul className = "list-group">
+                            <li className = "list-group-item" > Id: {id}</li>
+                            <li className = "list-group-item" > Nombre: {name}</li>
+                        </ul>
+                        {/* Se dejan los datos de cliente y compañia como filas por lo que se asigna el row y las col para las columnas */}
+                        <div className="row my-3">
+                            <div className="col">
+                                <h3> Datos del Cliente </h3>
+                                <ul className = "list-group">
+                                    <li className = "list-group-item active">Nombre del cliente: {nameClient} {lastName}</li>
+                                    <li className = "list-group-item">Dirección: {street} {number}, {city} de {country}</li>
+                                </ul>
+                            </div>
+                            <div className="col">
+                                <h3> Datos de Compañia</h3>
+                                <ul className = "list-group">
+                                    <li className = "list-group-item active">Nombre de la Compañia: {nameCompany}</li>
+                                    <li className = "list-group-item">Número Fiscal: {fiscalNumber}</li>
+                                </ul>
+                            </div>
+                        </div>
+                        <h4> Productos de la Factura</h4>
+                        <table className="table table-striped table-hover">
+                            {/* thead delimita los encabezados de la tabla */}
+                            <thead>
+                                <tr>
+                                    {/* th define las columnas */}
+                                    <th>Nombre del Producto</th>
+                                    <th>Precio del Producto</th>
+                                    <th>Cantidad de Productos disponibles</th>
+                                </tr>
+                            </thead>
+                            {/* tbody delimita los datos que no son el encabezado */}
+                            <tbody>
+                                {/* Se crea un map con las variables deben ir dentro de({}). Tambien se puede usar llamando
+                                una variable x por ejemplo items.map (item => y el return pero dentro de las celdas se deben llamar como
+                                item.producto, item.price.....) */}
+                                {items.map(({id,producto,price,quantity}) =>{
+                                    return (
+                                        <tr key={ id }>
+                                            <td>{producto}</td>
+                                            <td>{price}</td>
+                                            <td>{quantity}</td>
+                                        </tr>
+                                    );
+                                })}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>    
+            </div>
         </>
     );
 }
